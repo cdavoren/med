363534188da27ce9440eebed4d7e18c5d9c10a8f
Chris Davoren <chris@rubikscomplex.net>
2012-09-09 16:32:25 +1000

    Initial commit of files.

---
+++ template/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<?php if (isset($page_title)) : ?>
+<title><?php echo $page_title ?></title>
+<?php endif; ?>
+<script type="text/javascript" source="script/jquery-1.8.1.js"></script>
+<link rel="stylesheet" type="text/css" href="css/yui-3.6.0-reset-fonts-base.css" />
+<link rel="stylesheet" type="text/css" href="css/common.css" />
+<style>
+</style>
+</head>
+<body>
---
+++ www/index.php
@@ -0,0 +1,10 @@
+<?php 
+$page_title = 'Index Page'; 
+?>
+
+<?php require_once('../template/header.php'); ?>
+
+<h1>TEST</h1>
+<p>This is body text in a &lt;p&gt; tag.</p>
+
+<?php require_once('../template/footer.php'); ?>
