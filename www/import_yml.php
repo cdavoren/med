0c9387191ff69bc77de9ddb2443b47cd6f6c7f78
Chris Davoren <cdavoren@gmail.com>
2012-09-20 08:25:53 +1000

    Added: YML import script ability to import all files in a directory (simply omit filename parameter).

--- www/import_yml.php
+++ www/import_yml.php
@@ -12,10 +12,11 @@ use rubikscomplex\util\Importer;
 $pageTitle = 'Import Test YML';
 include('../template/header.php');
 
+printf('&nbsp;');
 printf('<h1>IMPORT TEST YML</h1>');
 
-if (!isset($_REQUEST['filename'])) {
-    printf('<p class="error">Error: No filename specified.</p>');
+if (!isset($_REQUEST['filename']) && !isset($_REQUEST['dir'])) {
+    printf('<p class="error">Error: No filename or directory specified.</p>');
 }
 else {
     if (isset($_REQUEST['clear'])) {
@@ -26,21 +27,30 @@ else {
         $em->flush();
     }
 
+    $filenames = array();
+    if (isset($_REQUEST['dir']) && !isset($_REQUEST['filename'])) {
+        $filenames = glob($_REQUEST['dir'].DIRECTORY_SEPARATOR.'*.yml');
+    }
+    else {
+        $filenames[] = $_REQUEST['filename'];
+    }
+
     $dir = isset($_REQUEST['dir']) ? $_REQUEST['dir'] : '';
-    $filename = $dir.DIRECTORY_SEPARATOR.$_REQUEST['filename'];
 
     $debug = array();
     $debug['output'] = '';
 
-    $error = Importer::importYML($filename, $em, $debug);
+    foreach ($filenames as $filename) {
+        printf('<p>Parsing %s...</p>', $filename);
+        $error = Importer::importYML($filename, $em, $debug);
 
-    if ($error !== null) {
-        printf('<p class="error">Error importing file <i>\'%s\'</i>: %s</p>', $filename, $error);
+        if ($error !== null) {
+            printf('<p class="error">Error importing file <i>\'%s\'</i>: %s</p>', $filename, $error);
+        }
+        else {
+            printf('<p class="success">Successfully imported file <i>\'%s\'</i>.</p>', $filename);
+        }
     }
-    else {
-        printf('<p class="success">Successfully imported file <i>\'%s\'</i>.</p>', $filename);
-    }
-
     printf('<h2>Debug:</h2>');
     printf($debug['output']);
 }
