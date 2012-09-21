2442d65d38875a335a0901527ec7539ebad94682
Chris Davoren <cdavoren@gmail.com>
2012-09-21 14:48:28 +1000

    Added: Footer with rubikscomplex 'logo' and removed SASS cache directory from repository.

--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,14 @@
 </div>
+<div id="footer">
+<div class="footerinner">
+<div class="footerlinks">
+<a href="mailto:admin@rubikscomplex.net" title="Contact Administrator">Contact</a> | <a href="#" title="About">About</a>
+</div>
+<div class="footerlogo">
+<span class="logofirst">RUBIKS</span><span class="logosecond">COMPLEX</span>&nbsp;<img class="logoimg" src="images/boxnotch-blue-14.png"/>
+</div>
+<div class="footerclear"></div>
+</div>
+</div>
 </body>
 </html>
