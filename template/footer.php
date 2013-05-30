6c873e5f1554788da41d4d41ec621523835565bf
Chris Davoren <cdavoren@gmail.com>
2013-05-30 15:12:13 +1000

    Fixed: Footer email address now uses admin email from config.php

--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,13 @@
+<?php
+
+$config = App::getConfiguration();
+
+?>
 </div>
 <div id="footer">
 <div class="footerinner">
 <div class="footerlinks">
-<a href="mailto:admin@rubikscomplex.net" title="Contact Administrator">Contact</a> | <a href="#" title="About">About</a>
+<a href="mailto:<?php echo $config['admin_email'] ?>" title="Contact Administrator">Contact</a> | <a href="#" title="About">About</a>
 </div>
 <div class="footerlogo">
 <span class="logofirst">RUBIKS</span><span class="logosecond">COMPLEX</span>&nbsp;<img class="logoimg" src="<?php echo $pathPrefix ?>images/boxnotch-blue-14.png"/>
